f14cfb67e9970e0c4a4e27f47a2efe323dad3e16
baranacikgoz <baran-acikgoz@outlook.com>
2023-11-10 01:10:04 +0300

    refactor: Move request validators inside request files

--- src/Modules/IdentityAndAuth/Features/Tokens/Refresh/Request.cs
+++ src/Modules/IdentityAndAuth/Features/Tokens/Refresh/Request.cs
@@ -1,6 +1,23 @@
 ﻿using Common.Core.Contracts.Results;
 using NimbleMediator.Contracts;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
 
 namespace IdentityAndAuth.Features.Tokens.Refresh;
 
 public sealed record Request(string ExpiredAccessToken, string RefreshToken) : IRequest<Result<Response>>;
+
+public sealed class RequestValidator : AbstractValidator<Request>
+{
+    public RequestValidator(IStringLocalizer<RequestValidator> localizer)
+    {
+        RuleFor(x => x.RefreshToken)
+            .NotEmpty()
+            .WithMessage(localizer["Yenileme tokeni boş olamaz."]);
+
+        RuleFor(x => x.ExpiredAccessToken)
+            .NotEmpty()
+            .WithMessage(localizer["Eski erişim tokeni boş olamaz."]);
+
+    }
+}
