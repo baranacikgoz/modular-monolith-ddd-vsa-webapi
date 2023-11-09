b839fa2291d4953b6065ddc339cd0ef5f769d686
baranacikgoz <baran-acikgoz@outlook.com>
2023-11-10 01:10:04 +0300

    refactor: Move request validators inside request files

--- src/Modules/IdentityAndAuth/Features/Users/ProvePhoneOwnership/Request.cs
+++ src/Modules/IdentityAndAuth/Features/Users/ProvePhoneOwnership/Request.cs
@@ -1,6 +1,21 @@
 ﻿using Common.Core.Contracts.Results;
 using NimbleMediator.Contracts;
+using FluentValidation;
+using IdentityAndAuth.Features.Common.Validations;
+using Microsoft.Extensions.Localization;
 
 namespace IdentityAndAuth.Features.Users.ProvePhoneOwnership;
 
 public sealed record Request(string PhoneNumber, string Otp) : IRequest<Result<Response>>;
+
+public class RequestValidator : AbstractValidator<Request>
+{
+    public RequestValidator(IStringLocalizer<RequestValidator> localizer)
+    {
+        RuleFor(x => x.PhoneNumber)
+            .PhoneNumberValidation(localizer);
+
+        RuleFor(x => x.Otp)
+            .OtpValidation(localizer);
+    }
+}
--- src/Modules/IdentityAndAuth/Features/Users/SelfRegister/Request.cs
+++ src/Modules/IdentityAndAuth/Features/Users/SelfRegister/Request.cs
@@ -1,5 +1,10 @@
 ﻿using Common.Core.Contracts.Results;
 using NimbleMediator.Contracts;
+using System.Globalization;
+using FluentValidation;
+using IdentityAndAuth.Features.Common.Validations;
+using IdentityAndAuth.Features.Users.Domain;
+using Microsoft.Extensions.Localization;
 
 namespace IdentityAndAuth.Features.Users.SelfRegister;
 
@@ -11,3 +16,52 @@ public sealed record Request(
         string NationalIdentityNumber,
         string BirthDate)
         : IRequest<Result<Response>>;
+
+public sealed class RequestValidator : AbstractValidator<Request>
+{
+    private const char EmptySpace = ' ';
+    private static readonly HashSet<char> _turkishAlphabetSet = new("abcçdefgğhıijklmnoöprsştuüvyzABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ");
+
+    public RequestValidator(IStringLocalizer<RequestValidator> localizer)
+    {
+        RuleFor(x => x.PhoneVerificationToken)
+            .PhoneVerificationTokenValidation(localizer);
+
+        RuleFor(x => x.PhoneNumber)
+            .PhoneNumberValidation(localizer);
+
+        RuleFor(x => x.FirstName)
+            .NotEmpty()
+                .WithMessage(localizer["İsim boş olamaz."])
+            .Must(str => str.All(c => IsEligibleForFirstName(c)))
+                .WithMessage(localizer["İsim sadece Türkçe alfabesindeki karakterlerden oluşabilir."])
+            .MaximumLength(ApplicationUserConstants.FirstNameMaxLength)
+                .WithMessage(localizer["İsim {0} karakterden uzun olamaz.", ApplicationUserConstants.FirstNameMaxLength]);
+
+        RuleFor(x => x.LastName)
+            .NotEmpty()
+                .WithMessage(localizer["Soyisim boş olamaz."])
+            .Must(str => str.All(c => _turkishAlphabetSet.Contains(c)))
+                .WithMessage(localizer["Soyisim sadece Türkçe alfabesindeki karakterlerden oluşabilir ve boşluk içermemelidir."])
+            .MaximumLength(ApplicationUserConstants.LastNameMaxLength)
+                .WithMessage(localizer["Soyisim {0} karakterden uzun olamaz.", ApplicationUserConstants.LastNameMaxLength]);
+
+        RuleFor(x => x.NationalIdentityNumber)
+            .NotEmpty()
+                .WithMessage(localizer["T.C. Kimlik numarası boş olamaz."])
+            .Length(ApplicationUserConstants.NationalIdentityNumberLength)
+                .WithMessage(localizer["T.C. Kimlik numarası {0} karakter olmalıdır.", ApplicationUserConstants.NationalIdentityNumberLength])
+            .Must(str => str.All(char.IsDigit))
+                .WithMessage(localizer["T.C. Kimlik numarası sadece rakamlardan oluşabilir."]);
+
+        RuleFor(x => x.BirthDate)
+            .NotEmpty()
+                .WithMessage(localizer["Doğum tarihi boş olamaz."])
+            .Must(str => DateOnly.TryParse(str, CultureInfo.InvariantCulture, DateTimeStyles.None, out _))
+                .WithMessage(localizer["Doğum tarihi, yyyy-MM-dd formatında olmalıdır."]);
+    }
+
+    // Empty space is included because we want to allow names with middle names.
+    private static bool IsEligibleForFirstName(char c)
+        => _turkishAlphabetSet.Contains(c) || c.Equals(EmptySpace);
+}
