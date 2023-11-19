ad2636cad1e23331e3abb68fcae144b93f91a346
baranacikgoz <baran-acikgoz@outlook.com>
2023-11-19 21:05:40 +0300

    fix(result): fix async task always run regardless result failure bug

--- src/Common/Common.Core/Contracts/Results/Result.cs
+++ src/Common/Common.Core/Contracts/Results/Result.cs
@@ -47,6 +47,11 @@ public sealed class Result<T>
     }
 
 #pragma warning disable CA1000
+    public static async Task<Result<T>> Create(Task<T?> taskToAwaitValue, Error errorIfValueNull)
+    {
+        var value = await taskToAwaitValue.ConfigureAwait(false);
+        return value is null ? Failure(errorIfValueNull) : Success(value);
+    }
     public static Result<T> Success(T value) => new(value);
     public static Result<T> Failure(Error error) => new(error);
 #pragma warning restore CA1000
@@ -54,5 +59,6 @@ public sealed class Result<T>
 #pragma warning disable CA2225
     public static implicit operator Result<T>(T value) => Success(value);
     public static implicit operator Result<T>(Error error) => Failure(error);
+    public static implicit operator Result(Result<T> result) => result.IsSuccess ? Result.Success : Result.Failure(result.Error!);
 #pragma warning restore CA2225
 }
