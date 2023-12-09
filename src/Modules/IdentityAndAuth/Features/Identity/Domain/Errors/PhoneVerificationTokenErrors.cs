44c24c8de1c429b40915188fb8a0f6600d9b326f
baranacikgoz <baran-acikgoz@outlook.com>
2023-12-09 22:17:46 +0300

    chore: rename async method name

--- src/Modules/IdentityAndAuth/Features/Identity/Domain/Errors/PhoneVerificationTokenErrors.cs
+++ src/Modules/IdentityAndAuth/Features/Identity/Domain/Errors/PhoneVerificationTokenErrors.cs
@@ -1,9 +1,10 @@
+using System.Net;
 using Common.Core.Contracts.Results;
 
 namespace IdentityAndAuth.Features.Identity.Domain.Errors;
 
 internal static class PhoneVerificationTokenErrors
 {
-    public static readonly Error TokenNotFound = new(nameof(TokenNotFound));
+    public static readonly Error TokenNotFound = new(nameof(TokenNotFound), HttpStatusCode.NotFound);
     public static readonly Error NotMatching = new(nameof(NotMatching));
 }
