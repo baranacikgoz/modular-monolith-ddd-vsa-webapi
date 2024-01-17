a1231baa1b280fd0107bb190558c35ee83fa7bad
baranacikgoz <baran-acikgoz@outlook.com>
2024-01-18 00:14:39 +0300

    refactor(migration): apply migration for field name change inside ApplicationUser.cs

--- src/Modules/IdentityAndAuth/Persistence/Migrations/20240116165954_renamed_name_property.Designer.cs
+++ src/Modules/IdentityAndAuth/Persistence/Migrations/20240116165954_renamed_name_property.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IdentityAndAuth.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace IdentityAndAuth.Persistence.Migrations
 {
     [DbContext(typeof(IdentityContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20240116165954_renamed_name_property")]
+    partial class renamed_name_property
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Modules/IdentityAndAuth/Persistence/Migrations/20240116165954_renamed_name_property.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityAndAuth.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class renamed_name_property : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                schema: "IdentityAndAuth",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                schema: "IdentityAndAuth",
+                table: "Users",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                schema: "IdentityAndAuth",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                schema: "IdentityAndAuth",
+                table: "Users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
