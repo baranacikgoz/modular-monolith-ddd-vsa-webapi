2db37f093d8266ebc3d3a0523546f750ce6aa6eb
baranacikgoz <baran-acikgoz@outlook.com>
2024-01-23 15:36:39 +0300

    perf(ProblemDetailsFactory): use Object Pooling to improve perf and reduce memory footprint

--- src/Host/Infrastructure/ProblemDetailsFactory.cs
+++ src/Host/Infrastructure/ProblemDetailsFactory.cs
@@ -1,29 +1,51 @@
 using Common.Core.Interfaces;
+using Microsoft.Extensions.ObjectPool;
 
 namespace Host.Infrastructure;
 
-public class ProblemDetailsFactory : IProblemDetailsFactory
+public class ProblemDetailsFactory(ObjectPool<CustomProblemDetails> problemDetailsPool) : IProblemDetailsFactory
 {
     public IResult Create(int status, string title, string type, string instance, string requestId, IEnumerable<string> errors)
-        => new CustomProblemDetails
+    {
+        var problemDetails = problemDetailsPool.Get();
+        try
         {
-            Status = status,
-            Title = title,
-            Type = type,
-            Instance = instance,
-            RequestId = requestId,
-            Errors = errors
-        };
+            problemDetails.ReInitialize(
+                status: status,
+                title: title,
+                type: type,
+                instance: instance,
+                requestId: requestId,
+                errors: errors);
+
+            return problemDetails;
+        }
+        finally
+        {
+            problemDetailsPool.Return(problemDetails);
+        }
+    }
 }
 
-internal class CustomProblemDetails : IResult
+public class CustomProblemDetails : IResult, IResettable
 {
-    public required int Status { get; init; }
-    public required string Title { get; init; }
-    public required string Type { get; init; }
-    public required string Instance { get; init; }
-    public required string RequestId { get; init; }
-    public required IEnumerable<string> Errors { get; init; }
+    public int Status { get; private set; }
+    public string Title { get; private set; } = string.Empty;
+    public string Type { get; private set; } = string.Empty;
+    public string Instance { get; private set; } = string.Empty;
+    public string RequestId { get; private set; } = string.Empty;
+    private readonly List<string> _errors = [];
+    public IReadOnlyCollection<string> Errors => _errors;
+
+    public void ReInitialize(int status, string title, string type, string instance, string requestId, IEnumerable<string> errors)
+    {
+        Status = status;
+        Title = title;
+        Type = type;
+        Instance = instance;
+        RequestId = requestId;
+        _errors.AddRange(errors);
+    }
 
     public Task ExecuteAsync(HttpContext httpContext)
     {
@@ -31,6 +53,12 @@ internal class CustomProblemDetails : IResult
         httpContext.Response.ContentType = "application/json";
 
         return httpContext.Response.WriteAsJsonAsync(this);
+    }
+
+    public bool TryReset()
+    {
+        _errors.Clear();
+        return true;
     }
 }
 
