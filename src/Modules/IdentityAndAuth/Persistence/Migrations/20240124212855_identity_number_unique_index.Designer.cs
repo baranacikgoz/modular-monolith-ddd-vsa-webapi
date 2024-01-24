997f463e9941a324aee12c16cab9e21a65473a32
baranacikgoz <baran-acikgoz@outlook.com>
2024-01-25 00:29:42 +0300

    feat(migration): apply migration for new index on national identity number
    BREAKING CHANGE:

--- src/Modules/IdentityAndAuth/Persistence/Migrations/20240124212855_identity_number_unique_index.Designer.cs
+++ src/Modules/IdentityAndAuth/Persistence/Migrations/20240124212855_identity_number_unique_index.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IdentityAndAuth.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace IdentityAndAuth.Persistence.Migrations
 {
     [DbContext(typeof(IdentityDbContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20240124212855_identity_number_unique_index")]
+    partial class identity_number_unique_index
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Modules/IdentityAndAuth/Persistence/Migrations/20240124212855_identity_number_unique_index.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityAndAuth.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class identity_number_unique_index : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "IdentityAndAuth",
+                table: "Users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_NationalIdentityNumber",
+                schema: "IdentityAndAuth",
+                table: "Users",
+                column: "NationalIdentityNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_NationalIdentityNumber",
+                schema: "IdentityAndAuth",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "IdentityAndAuth",
+                table: "Users",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
