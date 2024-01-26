94ce8b9f8a5cab11a11f2dfe27e1da75ebd65a96
baranacikgoz <baran-acikgoz@outlook.com>
2024-01-27 00:07:41 +0300

    feat(Migrations): apply optimistic concurrency migration

---
+++ src/Modules/Appointments/Persistence/Migrations/20240126210320_optimistic_concurrency.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Appointments.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class optimistic_concurrency : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                schema: "Appointments",
+                table: "Venues",
+                type: "bytea",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                schema: "Appointments",
+                table: "Appointments",
+                type: "bytea",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                schema: "Appointments",
+                table: "Venues");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                schema: "Appointments",
+                table: "Appointments");
+        }
+    }
+}
