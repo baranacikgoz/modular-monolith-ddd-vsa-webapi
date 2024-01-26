aac440acd6a4dd57fdd6882be981f736ec5c0e62
baranacikgoz <baran-acikgoz@outlook.com>
2024-01-27 00:01:22 +0300

    feat(concurrency): introduce optimistic concurrency

--- src/Common/Common.Persistence/BaseDbContext.cs
+++ src/Common/Common.Persistence/BaseDbContext.cs
@@ -2,13 +2,15 @@
 using Common.Core.Contracts;
 using Common.Eventbus;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Common.Persistence;
 
-public abstract class BaseDbContext(
+public abstract partial class BaseDbContext(
     DbContextOptions options,
     ICurrentUser currentUser,
-    IEventBus eventBus
+    IEventBus eventBus,
+    ILogger logger
     ) : DbContext(options)
 {
     public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
@@ -39,8 +41,30 @@ public abstract class BaseDbContext(
             }
         }
 
-        // Persist changes to the database
-        var result = await base.SaveChangesAsync(cancellationToken);
+        int result;
+        try
+        {
+            result = await base.SaveChangesAsync(cancellationToken);
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            if (ex.Entries.Count > 1)
+            {
+                var typeAndTableNames = ex.Entries.Select(e => MergeTypeAndTableName(
+                                                                e.Entity.GetType().Name,
+                                                                e.Metadata.GetTableName()));
+
+                LogConcurrencyExceptionOccuredOnMultipleEntities(logger, ex, typeAndTableNames, userId);
+                throw;
+            }
+
+            var typeAndTableName = MergeTypeAndTableName(
+                    ex.Entries.Single().Entity.GetType().Name,
+                    ex.Entries.Single().Metadata.GetTableName());
+
+            LogConcurrencyExceptionOccuredOnSingleEntity(logger, ex, typeAndTableName, userId);
+            throw;
+        }
 
         if (domainEvents?.Count > 0)
         {
@@ -52,4 +76,25 @@ public abstract class BaseDbContext(
 
         return result;
     }
+
+    private static string MergeTypeAndTableName(string? typeName, string? tableName)
+        => $"{typeName ?? "N/A"} / ({tableName ?? "N/A"})";
+
+    [LoggerMessage(
+        Level = LogLevel.Information,
+        Message = "Concurrency exception occurred on the type/table {TypeAndTableName} for the user {UserId}.")]
+    static partial void LogConcurrencyExceptionOccuredOnSingleEntity(
+        ILogger logger,
+        DbUpdateConcurrencyException exception,
+        string TypeAndTableName,
+        Guid UserId);
+
+    [LoggerMessage(
+        Level = LogLevel.Information,
+        Message = "Concurrency exception occurred on the types/tables {@TypeAndTableNames} for the user {UserId}.")]
+    static partial void LogConcurrencyExceptionOccuredOnMultipleEntities(
+        ILogger logger,
+        DbUpdateConcurrencyException exception,
+        IEnumerable<string> TypeAndTableNames,
+        Guid UserId);
 }
