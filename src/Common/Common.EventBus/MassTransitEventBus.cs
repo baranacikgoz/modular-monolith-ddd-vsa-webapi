246d7cec669e77737c511293a45cf95865ae2935
baranacikgoz <baran-acikgoz@outlook.com>
2024-02-06 22:18:35 +0300

    feat(MassTransitEventbus): add more logging, before and after publishing events

--- src/Common/Common.EventBus/MassTransitEventBus.cs
+++ src/Common/Common.EventBus/MassTransitEventBus.cs
@@ -12,12 +12,22 @@ public partial class MassTransitEventBus(
 {
     public async Task PublishAsync<TEvent>(TEvent @event, CancellationToken cancellationToken = default) where TEvent : DomainEvent
     {
+        var eventName = @event.GetType().Name;
+
+        LogPublishingEvent(logger, eventName);
+
         await bus.Publish(@event, cancellationToken);
-        LogEventPublished(logger, @event.GetType().Name);
+
+        LogPublishedEvent(logger, eventName);
     }
 
     [LoggerMessage(
         Level = LogLevel.Debug,
-        Message = "Event {EventName} published successfully.")]
-    private static partial void LogEventPublished(ILogger logger, string eventName);
+        Message = "Publishing {EventName} event...")]
+    private static partial void LogPublishingEvent(ILogger logger, string eventName);
+
+    [LoggerMessage(
+        Level = LogLevel.Debug,
+        Message = "Published {EventName} event successfully.")]
+    private static partial void LogPublishedEvent(ILogger logger, string eventName);
 }
