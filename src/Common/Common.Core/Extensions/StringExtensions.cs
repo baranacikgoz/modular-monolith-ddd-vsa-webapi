b78d108bbcaecf7704ead5276c777f2d680bf8ff
baranacikgoz <baran-acikgoz@outlook.com>
2024-02-08 00:45:55 +0300

    refactor: decouple turkish alphabet check so other modules can use too

--- src/Common/Common.Core/Extensions/StringExtensions.cs
+++ src/Common/Common.Core/Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Common.Core.Contracts.Results;
+﻿using System.Buffers;
+using Common.Core.Contracts.Results;
 
 namespace Common.Core.Extensions;
 
@@ -8,4 +9,21 @@ public static class StringExt
         => string.IsNullOrEmpty(value)
             ? Result<string>.Failure(ifNullOrEmpty)
             : Result<string>.Success(value!);
+
+    private static readonly SearchValues<char> _turkishAlphabet = SearchValues.Create("abcçdefgğhıijklmnoöprsştuüvyzABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ");
+    private static readonly SearchValues<char> _turkishAlphabetWithEmptySpace = SearchValues.Create(" abcçdefgğhıijklmnoöprsştuüvyzABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ");
+    public static bool ContainsOnlyTurkishCharacters(this string s, bool allowWhiteSpace)
+    {
+        if (string.IsNullOrEmpty(s))
+        {
+            return false;
+        }
+
+        if (allowWhiteSpace)
+        {
+            return !s.AsSpan().ContainsAnyExcept(_turkishAlphabetWithEmptySpace);
+        }
+
+        return !s.AsSpan().ContainsAnyExcept(_turkishAlphabet);
+    }
 }
