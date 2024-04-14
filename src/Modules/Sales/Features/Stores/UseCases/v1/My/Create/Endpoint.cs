4902ee123ca670e3db9de30b868d1bda30dd1ece
baranacikgoz <baran-acikgoz@outlook.com>
2024-04-14 18:17:30 +0300

    feat(stores/my/create): implement usecase

---
+++ src/Modules/Sales/Features/Stores/UseCases/v1/My/Create/Endpoint.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Common.Core.Auth;
+using Common.Core.Contracts.Results;
+using Microsoft.AspNetCore.Mvc;
+using Common.Core.Extensions;
+using Sales.Persistence;
+using Sales.Features.Stores.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sales.Features.Stores.UseCases.v1.My.Create;
+
+internal static class Endpoint
+{
+    internal static void MapEndpoint(RouteGroupBuilder salesApiGroup)
+    {
+        salesApiGroup
+            .MapPost("", CreateMyStoreAsync)
+            .WithDescription("Create my store.")
+            .MustHavePermission(CustomActions.Create, CustomResources.Stores)
+            .Produces<Response>(StatusCodes.Status200OK)
+            .TransformResultTo<Response>();
+    }
+
+    private static async ValueTask<Result<Response>> CreateMyStoreAsync(
+        [FromBody] Request request,
+        [FromServices] ICurrentUser currentUser,
+        [FromServices] SalesDbContext dbContext,
+        CancellationToken cancellationToken)
+    {
+        var ownerId = currentUser.Id;
+        var store = Store.Create(ownerId, request.Name);
+
+        dbContext.Stores.Add(store);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return Result<Response>.Success(new Response(store.Id.Value));
+    }
+}
---
+++ src/Modules/Sales/Features/Stores/UseCases/v1/My/Create/Request.cs
@@ -0,0 +1,3 @@
+namespace Sales.Features.Stores.UseCases.v1.My.Create;
+
+internal sealed record Request(string Name);
