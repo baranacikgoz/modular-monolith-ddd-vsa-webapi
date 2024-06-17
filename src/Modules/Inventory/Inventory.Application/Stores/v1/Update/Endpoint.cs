d3e1dc4f6ea78b0efd7fa56090d9b08e8861c002
baranacikgoz <baran-acikgoz@outlook.com>
2024-06-18 01:23:27 +0300

    feat(UpdateStore): implement endpoint

---
+++ src/Modules/Inventory/Inventory.Application/Stores/v1/Update/Endpoint.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc;
+using Common.Application.Auth;
+using Common.Domain.ResultMonad;
+using Common.Application.Extensions;
+using Common.Domain.StronglyTypedIds;
+using Common.Application.ModelBinders;
+using Ardalis.Specification;
+using Inventory.Domain.Stores;
+using Microsoft.Extensions.DependencyInjection;
+using Common.Application.Persistence;
+
+namespace Inventory.Application.Stores.v1.Update;
+
+internal static class Endpoint
+{
+    internal static void MapEndpoint(RouteGroupBuilder myStoresApiGroup)
+    {
+        myStoresApiGroup
+            .MapPut("{id}", CreateStoreAsync)
+            .WithDescription("Update a store.")
+            .MustHavePermission(CustomActions.Update, CustomResources.Stores)
+            .Produces(StatusCodes.Status204NoContent)
+            .TransformResultToNoContentResponse();
+    }
+
+    private sealed class StoreByIdSpec : SingleResultSpecification<Store>
+    {
+        public StoreByIdSpec(StoreId id)
+            => Query
+                .Where(s => s.Id == id);
+    }
+
+    private static async Task<Result> CreateStoreAsync(
+        [FromRoute, ModelBinder<StronglyTypedIdBinder<StoreId>>] StoreId id,
+        [FromBody] Request request,
+        [FromServices] IRepository<Store> repository,
+        [FromKeyedServices(nameof(Inventory))] IUnitOfWork unitOfWork,
+        CancellationToken cancellationToken)
+        => await repository.SingleOrDefaultAsResultAsync(new StoreByIdSpec(id), cancellationToken)
+            .TapAsync(store => store.Update(request.Name, request.Description))
+            .TapAsync(async _ => await unitOfWork.SaveChangesAsync(cancellationToken));
+}
