fecb3b06f47b52716167ac38607609f58a963772
baranacikgoz <baran-acikgoz@outlook.com>
2024-06-17 22:45:31 +0300

    feat(Store): add unique index to owner
    a user can have only one store

---
+++ src/Modules/Inventory/Inventory.Infrastructure/Persistence/Migrations/20240617194426_StoreOwnerUniqueIndex.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class StoreOwnerUniqueIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Stores_OwnerId",
+                schema: "Inventory",
+                table: "Stores",
+                column: "OwnerId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Stores_OwnerId",
+                schema: "Inventory",
+                table: "Stores");
+        }
+    }
+}
