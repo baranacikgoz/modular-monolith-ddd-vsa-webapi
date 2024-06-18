689df22588a2f48a63d22f6e7864905c12a8b59c
baranacikgoz <baran-acikgoz@outlook.com>
2024-06-18 21:03:19 +0300

    feat(CreateMyStore): implement endpoint

--- src/Modules/Inventory/Inventory.Application/Stores/v1/My/Create/Request.cs
+++ src/Modules/Inventory/Inventory.Application/Stores/v1/My/Create/Request.cs
@@ -1,3 +1,27 @@
+using Common.Application.Localization;
+using Common.Application.Validation;
+using Common.Domain.StronglyTypedIds;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+
 namespace Inventory.Application.Stores.v1.My.Create;
 
-internal sealed record Request(string Name);
+public sealed record Request(string Name, string Description);
+
+public sealed class RequestValidator : CustomValidator<Request>
+{
+    public RequestValidator(IStringLocalizer<ResxLocalizer> localizer)
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+                .WithMessage(localizer["Stores.v1.Create.Name.NotEmpty"])
+            .MaximumLength(Domain.Stores.Constants.NameMaxLength)
+                .WithMessage(localizer["Stores.v1.Create.Name.MaxLength {0}", Domain.Stores.Constants.NameMaxLength]);
+
+        RuleFor(x => x.Description)
+            .NotEmpty()
+                .WithMessage(localizer["Stores.v1.Create.Description.NotEmpty"])
+            .MaximumLength(Domain.Stores.Constants.DescriptionMaxLength)
+                .WithMessage(localizer["Stores.v1.Create.Description.MaxLength {0}", Domain.Stores.Constants.DescriptionMaxLength]);
+    }
+}
